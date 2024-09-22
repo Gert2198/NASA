b73b7a2dd564ac6502e2f1c9c796cf1c848b328c
Gert2198 <sergiolmblizzard@gmail.com>
2024-09-22 14:34:40 +0200

    Update: reduced server request loads with our own .json maps

--- src/map.js
+++ src/map.js
@@ -1,35 +1,26 @@
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaW81NTcyIiwiYSI6ImNsbjdxYm5qbzBsYXYycG8ycXliamU3b3kifQ.4j-2bFW0uRXWsk36ji7w_Q';
-
-// Inicializar el mapa
-const map = new mapboxgl.Map({
-  container: 'map',
-  center: [-74.5, 40], // Coordenadas iniciales [longitud, latitud]
-  zoom: 9 // Nivel de zoom inicial
-});
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFyaW81NTcyIiwiYSI6ImNsbjdxYm5qbzBsYXYycG8ycXliamU3b3kifQ.4j-2bFW0uRXWsk36ji7w_Q";
 
 // Añadir controles de navegación (zoom y rotación)
 map.addControl(new mapboxgl.NavigationControl());
 
 // Crear un elemento HTML para el marcador
 const marker = new mapboxgl.Marker({
-  draggable: true // Hacer que el marcador sea draggable
+  draggable: true, // Hacer que el marcador sea draggable
 })
-.setLngLat([-74.5, 40]) // Posición inicial del marcador
-.addTo(map);
-
-// Obtener el elemento donde mostraremos las coordenadas
-const coordinates = document.getElementById('coordinates');
+  .setLngLat([-74.5, 40]) // Posición inicial del marcador
+  .addTo(map);
 
 // Función para actualizar las coordenadas en el DOM
 function onDragEnd() {
   const lngLat = marker.getLngLat();
-  console.log(`Latitud: ${lngLat.lat.toFixed(5)}, Longitud: ${lngLat.lng.toFixed(5)}`)
+  console.log(
+    `Latitud: ${lngLat.lat.toFixed(5)}, Longitud: ${lngLat.lng.toFixed(5)}`
+  );
 }
 
-map.on('click', (e)=>{
-    console.log("Soy solo click")
-    marker.setLngLat(e.lngLat)
-})
+map.on("click", (e) => {
+  marker.setLngLat(e.lngLat);
+});
 // Escuchar el evento de arrastre final del marcador
-marker.on('dragend', onDragEnd);
+marker.on("dragend", onDragEnd);
--- src/modeSwitch.js
+++ src/modeSwitch.js
@@ -1,10 +1,36 @@
-const checkbox = document.getElementById('toggle');
+const checkbox = document.getElementById("toggle");
 const body = document.body;
+let lightMap
+let darkMap
+let map
 
-checkbox.addEventListener('change', () => {
+fetch('../styles/lightMap.json')
+  .then(response => response.json())  // Parsear el JSON automáticamente
+  .then(data => {
+    map = new mapboxgl.Map({
+      container: "map",
+      style: data,
+      center: [-74.5, 40], // Coordenadas iniciales [longitud, latitud]
+      zoom: 9, // Nivel de zoom inicial
+    });
+    lightMap = data;
+  })
+  .catch(error => console.error('Error:', error));
+
+fetch('../styles/darkMap.json')
+  .then(response => response.json())  // Parsear el JSON automáticamente
+  .then(data => {
+    darkMap = data;
+  })
+  .catch(error => console.error('Error:', error));
+
+checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
-    body.classList.add('dark-mode');
+    body.classList.add("dark-mode");
+    map.setStyle(darkMap)
   } else {
-    body.classList.remove('dark-mode');
+    body.classList.remove("dark-mode")
+    map.setStyle(lightMap)
+    console.log('NO JSON')
   }
 });
